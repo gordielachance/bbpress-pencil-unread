2e641f9ce55b06ad1af9d5738ad22cff8aa5302e
gordielachance <Gordie@mbp-de-gordie.home>
2016-09-09 11:50:54 +0200

    improved jQuery
    When marking a forum as read, give the 'bbppu-read' class only to the
    topics of that forum (super sticky topics could be from another forum
    so they should remain unread)

--- _inc/js/bbppu.js
+++ _inc/js/bbppu.js
@@ -3,8 +3,12 @@ jQuery(document).ready(function($){
     /*
     Mark as read when clicking a forum/topic; so if the user goes back with its browser, the forum/topic is not marked as unread.
     //TO FIX not working + maybe there is a better method to avoid this ?
+    
+    var forums = $('.bbppu-unread .bbp-forum-info a.bbp-forum-title');
+    var topics = $('.bbppu-unread .bbp-topic-title a.bbp-topic-permalink');
+    var items = $.merge(forums,topics);
 
-    $('.bbppu-unread .bbp-forum-info a.bbp-forum-title, .bbppu-unread .bbp-topic-title a.bbp-topic-permalink').click(function(event){
+    items.click(function(event){
         var item = $(this).closest(".bbppu-unread");
         item.removeClass("bbppu-unread").addClass("bbppu-read");
     });
@@ -41,7 +45,17 @@ jQuery(document).ready(function($){
                     console.log(data);
                 }else if (data.success === true) {
                     block.html(bbppuL10n.marked_as_read);
-                    var items = $('#forums-list-'+ajax_data.forum_id+' .bbppu-hentry, #bbp-forum-'+ajax_data.forum_id+' .bbppu-hentry');
+                    var forums = $('#forums-list-'+ajax_data.forum_id+' .bbppu-hentry');
+                    
+                    /*
+                    //this would also mark super sticky topics, which may be from other forums.
+                    var topics = $('#bbp-forum-'+ajax_data.forum_id+' .bbppu-hentry'); 
+                    */
+
+                    var topics = $('.bbppu-hentry.bbp-parent-forum-'+ajax_data.forum_id); 
+                    
+                    var items = $.merge(forums,topics);
+                    
                     items.removeClass('bbppu-unread').addClass('bbppu-read');
                 }
 
